cfe21650eee1167202067ce913a0901538ba3e92
Zoe Jones <P2423891@my365.dmu.ac.uk>
2021-09-19 00:27:04 +0200

    Photo renamer

--- nine.py
+++ nine.py
@@ -1,12 +1,12 @@
 import tkinter as tk
 from tkinter import ttk
 from PIL import Image,  ImageTk
-from os import listdir, remove, mkdir
+from os import listdir, remove, mkdir,rename
 from os.path import isfile, join, isdir
 import shutil
 import threading
-if isdir("DELETE") == False:
-    mkdir("DELETE")
+import re
+
 files = [f for f in listdir(".//") if isfile(join(".//", f))]
 images = []
 currentImage = 0
@@ -15,48 +15,49 @@ allowedExtensions = ["png",
                      "jpeg",
                      "gif",
                      "bmp"]
-allowedKeys = {"1":"Memes",
-               "2":"Kitties",
-               "3":"Other",
-               "4":"Trash",
-               "5":"Trash Lite",
-               "6":"Selfies",
-               "7":"Cute",
-               "8":"Cool",
-               "9":"Lewds",
-               "0":"Art",
-               "minus":"Justin",
-               "equal":"Useful",
-               "Delete":"DELETE"}
 
 for file in files:
     if file.split(".")[-1].lower() in allowedExtensions:
         images.append(file)
 
 win = tk.Tk()
-win.geometry("500x840")
-
-def move(src,dest):
-    shutil.move(src,dest)
+win.geometry("500x640")
 
 def keydown(e):
     global currentImage
-    if currentImage >= len(images):
+    if currentImage >= len(images)-1:
         image_label.config(image='',text="No more images!",font=("Arial", 30))
+        imagefilename.configure(text="")
+        newImageName.configure(state="disabled")
     else:
-        if e.keysym in allowedKeys:
-            shutil.move(f".\{images[currentImage]}",f".\{allowedKeys[e.keysym]}\{images[currentImage]}")
-            currentImage += 1
-            if currentImage < len(images):
-                imagefilename.configure(text=images[currentImage])
-                imagefilename.text = images[currentImage]
-                try:
-                    img2 = resizeImage(images[currentImage])
-                    image_label.configure(image=img2)
-                    image_label.image = img2
-                except Exception as e:
-                    image_label.config(image='',text="Error displaying image",font=("Arial", 25))
-                    print(e)
+        imageFileExt = images[currentImage].split(".")[-1:]
+        safeNewImageName = newImageName.get().replace("<","").replace(">","").replace("?","").replace(":","").replace("*","").replace("|","").replace("/","").replace("\\","").replace("\"","")
+        rename(images[currentImage], safeNewImageName+"."+imageFileExt[0])
+
+        currentImage += 1
+        if currentImage < len(images):
+            imagefilename.configure(text=images[currentImage])
+            imagefilename.text = images[currentImage]
+            try:
+                img2 = resizeImage(images[currentImage])
+                image_label.configure(image=img2)
+                image_label.image = img2
+
+                imageFileNameNoExt = ""
+                if len(images[currentImage].split(".")[:-1]) > 1:
+                    for word in images[currentImage].split(".")[:-1]:
+                        imageFileNameNoExt = imageFileNameNoExt + word + "."
+                    imageFileNameNoExt = imageFileNameNoExt[:-1]
+                else:
+                    imageFileNameNoExt = images[currentImage].split(".")[:-1][0]
+
+                newImageName.delete(0,'end')
+                newImageName.insert(0,imageFileNameNoExt)
+
+            except Exception as e:
+                image_label.config(image='',text="Error displaying image",font=("Arial", 25))
+                print(e)
+    newImageName.focus()
 
 
 def resizeImage(imageFileName):
@@ -82,12 +83,21 @@ def resizeImage(imageFileName):
     return img
 
 image_label = ttk.Label(win)
+imageFileNameNoExt = ""
 if len(images) > 0:
     imagefilename = ttk.Label(win,text=images[currentImage],font=("Arial", 20))
     try:
         img2 = resizeImage(images[currentImage])
         image_label.configure(image=img2)
         image_label.image = img2
+
+
+        if len(images[currentImage].split(".")[:-1]) > 1:
+            for word in images[currentImage].split(".")[:-1]:
+                imageFileNameNoExt = imageFileNameNoExt + word + "."
+            imageFileNameNoExt = imageFileNameNoExt[:-1]
+        else:
+            imageFileNameNoExt = images[currentImage].split(".")[:-1][0]
     except Exception as e:
         image_label.config(image='',text="Error displaying image",font=("Arial", 25))
         print(e)
@@ -98,25 +108,13 @@ else:
 
 imagefilename.pack()
 image_label.pack()
-info = ttk.Label(win,text="""
-Press the following keyboard keys to move to the
-corresponding folder:
-1: Memes
-2: Kitties
-3: Other
-4: Trash
-5: Trash Lite
-6: Selfies
-7: Cute
-8: Cool
-9: Lewds
-0: Art
--: Justin
-=: Useful
+info = ttk.Label(win,text="""Enter filename and press enter to save (excluding extensions)
+A file name can't contain any of the following characters:
+\ / : * ? " < > |""",font=("Arial", 11)).pack()
+newImageName = ttk.Entry(width=70,font=("Arial", 16))
+newImageName.pack()
+newImageName.focus()
+newImageName.insert(0,imageFileNameNoExt)
 
-Del: Delete
-""",font=("Arial", 11))
-info.place(x=0,y=540)
-
-win.bind("<Key>", keydown)
+win.bind("<Return>", keydown)
 win.mainloop()
